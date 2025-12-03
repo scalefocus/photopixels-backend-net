1ec81f237912b35fa324dce1c066a3307e0d119e
kole.vasilevski <kole.vasilevski@scalefocus.com>
2025-12-03 09:45:07 +0100

    Introduce Wolverine and upgrade Marten to v8.16.1
    Replaced Oakton with Wolverine for messaging and resilience, adding support for transient error handling and asynchronous message processing. Upgraded Marten to v8.16.1 across all projects, removing the deprecated Marten.CommandLine package.
    
    Enhanced `VideoService` with Wolverine-based messaging for video conversion tasks. Updated `appsettings.json` with database connection strings, admin credentials, and email configuration.
    
    Refactored imports, constructors, and configurations for improved maintainability. Added retry policies for database exceptions and updated dependencies, including `Npgsql` and `Microsoft.Extensions.Options`. Temporarily disabled `OptimizeArtifactWorkflow` for development.

--- src/SF.PhotoPixels.Infrastructure/DependencyInjection.cs
+++ src/SF.PhotoPixels.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using DbUp;
 using HeyRed.ImageSharp.Heif.Formats.Heif;
+using JasperFx;
+using JasperFx.Events.Projections;
 using Marten;
 using Marten.Events.Projections;
 using Microsoft.AspNetCore.Identity;
@@ -141,10 +143,10 @@ public static class DependencyInjection
             })
             .UseLightweightSessions();
 
-        if (!isDevelopment)
-        {
-            martenConfig.OptimizeArtifactWorkflow();
-        }
+        //if (!isDevelopment)
+        //{
+        //    martenConfig.OptimizeArtifactWorkflow();
+        //}
 
         new HeifConfigurationModule().Configure(Configuration.Default);
 
